7ff83f402ccb1ff19c8c6ec1c6a9460bde52b8cc
KIMmminju <beth517@naver.com>
2023-04-24 17:09:09 +0900

    0424_todolist

--- todolist/index.js
+++ todolist/index.js
@@ -1,8 +1,8 @@
 const todoInputElem = document.querySelector(".new-todo");
 const listElem = document.querySelector(".todo-list");
-const filtersElem = document.querySelector('.filters');
-const clearCompletedBtn = document.querySelector('.clear-completed');
-const todosCountElem = document.querySelector('.todo-count');
+const filtersElem = document.querySelector(".filters");
+const clearCompletedBtn = document.querySelector(".clear-completed");
+const todosCountElem = document.querySelector(".todo-count");
 
 // 이전에 저장된 목록 가져오기 . localStorage를 사용하여 새로 고침을 해도 목록이 그대로 남아있게
 // localStorage - 브라우저를 새로고침해도 남아있음, 여러 탭이 공유할 수 있음
@@ -11,7 +11,7 @@ const todosCountElem = document.querySelector('.todo-count');
 const savedTodos = localStorage.getItem("todos");
 const savedfilter = localStorage.getItem("filter");
 let todos = [];
-let filter = 'all';
+let filter = "all";
 
 if (savedTodos) {
   // localStorage에 저장된 savedTodos가 존재하면
@@ -19,25 +19,28 @@ if (savedTodos) {
   todos = JSON.parse(savedTodos);
 }
 
-if(savedfilter){
+if (savedfilter) {
   filter = savedfilter;
 }
 
-todoInputElem.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter' && !event.isComposing && todoInputElem.value !== '') {
-
+todoInputElem.addEventListener("keydown", function (event) {
+  if (
+    event.key === "Enter" &&
+    !event.isComposing &&
+    todoInputElem.value !== ""
+  ) {
     // 새로운 할 일 객체 생성
     const newTodo = {
       id: Date.now(), // 고유한 id 값으로 현재 시간 사용
       title: todoInputElem.value,
-      completed: false
+      completed: false,
     };
 
     // 할 일 목록에 추가
     todos.push(newTodo);
 
     // localStorage에 저장
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 
     // 화면 다시 그리기
     render();
@@ -45,11 +48,11 @@ todoInputElem.addEventListener('keydown', function (event) {
 });
 
 // clear-completed 이벤트
-clearCompletedBtn.addEventListener('click', () => {
-  todos = todos.filter(item => !item.completed);
+clearCompletedBtn.addEventListener("click", () => {
+  todos = todos.filter((item) => !item.completed);
 
   // localStorage에 저장
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 
   // 화면 다시 그리기
   render();
@@ -57,16 +60,16 @@ clearCompletedBtn.addEventListener('click', () => {
 
 // filter 이벤트 리스너 추가
 // 데이터 갱신
-filtersElem.addEventListener("click", function(event){
+filtersElem.addEventListener("click", function (event) {
   const targetElem = event.target;
   // 대상 찾기
   const href = targetElem.getAttribute("href");
-  if(href === "#/"){
-    filter = 'all';
-  } else if(href === "#/active"){
-    filter = 'active';
-  } else if(href === "#/completed"){
-    filter = 'completed';
+  if (href === "#/") {
+    filter = "all";
+  } else if (href === "#/active") {
+    filter = "active";
+  } else if (href === "#/completed") {
+    filter = "completed";
   }
   // 필터 영역 다시 그리기
   renderFilter();
@@ -77,7 +80,7 @@ filtersElem.addEventListener("click", function(event){
 // 이벤트가 발생하면(ex: click) --> 데이터를 변경하고(delete, toggle) --> 다시 그리기(render)
 listElem.addEventListener(
   // click 이벤트
-  "click", 
+  "click",
 
   // click 이벤트가 발생되면 실행될 함수
   function (event) {
@@ -85,7 +88,7 @@ listElem.addEventListener(
     const targetElem = event.target;
 
     // li 엘리먼트 찾기 : closest() 메서드를 사용해서 li를 찾는다.
-    const liElem = targetElem.closest('li[data-id]');
+    const liElem = targetElem.closest("li[data-id]");
 
     // 변경될 데이터(todo)를 찾기위한 id 정보
     const id = liElem.dataset.id;
@@ -93,19 +96,19 @@ listElem.addEventListener(
     // 삭제 버튼 클릭 시
     if (targetElem.classList.contains("destroy")) {
       // 데이터 변경 : id를 사용하여 배열 todos 에서 삭제할 todo를 제거
-      todos = todos.filter(todo => todo.id !== parseInt(id));
+      todos = todos.filter((todo) => todo.id !== parseInt(id));
 
       // 로컬 스토리에 데이터 갱신 : 옵션x
       localStorage.setItem("todos", JSON.stringify(todos));
 
       // 화면 다시 그리기
       render();
-    } 
+    }
 
     // 체크박스가 토글 시
     else if (targetElem.classList.contains("toggle")) {
       // 데이터 가져오기 : id를 사용하여 todo 데이터 가져오기
-      const todoItem = todos.find(todo => todo.id === parseInt(id));
+      const todoItem = todos.find((todo) => todo.id === parseInt(id));
 
       // 데이터 변경 : 가져온 todo 데이터에서 완료여부를 변경(반대 값으로) true -> false, false -> true
       todoItem.completed = !todoItem.completed;
@@ -114,7 +117,7 @@ listElem.addEventListener(
       localStorage.setItem("todos", JSON.stringify(todos));
 
       // 화면 다시 그리기
-      render(); 
+      render();
     }
   }
 );
@@ -125,12 +128,12 @@ function render() {
   let filteredTodos = [];
 
   // 필터 조건에 맞게 목록을 가공하고
-  if (filter === 'all') {
+  if (filter === "all") {
     filteredTodos = todos;
-  } else if (filter === 'active') {
-    filteredTodos = todos.filter(item => !item.completed);
-  } else if (filter === 'completed') {
-    filteredTodos = todos.filter(item => item.completed);
+  } else if (filter === "active") {
+    filteredTodos = todos.filter((item) => !item.completed);
+  } else if (filter === "completed") {
+    filteredTodos = todos.filter((item) => item.completed);
   }
   // 그 목록으로 화면을 그린다.
   filteredTodos.forEach((item, index) => {
@@ -138,7 +141,9 @@ function render() {
     listHTML += `
       <li data-id="${item.id}" class="${completedClass}">
         <div class="view">
-          <input class="toggle" type="checkbox" ${item.completed ? "checked" : ""}>
+          <input class="toggle" type="checkbox" ${
+            item.completed ? "checked" : ""
+          }>
           <label>${item.title}</label>
           <button class="destroy"></button>
         </div>
@@ -151,25 +156,29 @@ function render() {
   todosCountElem.innerHTML = `
     <strong>${items}</strong>
     items left
-  `
+  `;
   todoInputElem.value = "";
 }
 
-const renderFilter = function() {
+const renderFilter = function () {
   const filterHTML = `
   <li>
-    <a href="#/" class="${filter === 'all' ? 'selected' : ''}">All</a>
+    <a href="#/" class="${filter === "all" ? "selected" : ""}">All</a>
   </li>
   <li>
-    <a href="#/active" class="${filter === 'active' ? 'selected' : ''}">Active</a>
+    <a href="#/active" class="${
+      filter === "active" ? "selected" : ""
+    }">Active</a>
   </li>
   <li>
-    <a href="#/completed" class="${filter === 'completed' ? 'selected' : ''}">Completed</a>
+    <a href="#/completed" class="${
+      filter === "completed" ? "selected" : ""
+    }">Completed</a>
   </li>
   `;
 
   filtersElem.innerHTML = filterHTML;
-}
+};
 
 // 새로고침할때 이전에 저장된 목록 가져오기(window...)
 // 새로고침이 일어나면 로컬스토리에 저장된 데이터를 기반으로 다시 그려야하니까...
--- todolist/index_test.js
+++ todolist/index_test.js
@@ -1,172 +1,239 @@
 const TodoList = {
-    data: {
-        todos: [],
-        filter: 'all'
-    },
-    /**
-     * 최초 동작에 해야 할 일 
-     */
-    init() {
-        // 데이터 초기화
-        const savedTodos = localStorage.getItem("todos");
-        const savedFilter = localStorage.getItem("filter");
-        if (savedTodos) {
-            this.data.todos = JSON.parse(savedTodos);
-        }
+  data: {
+    todos: [],
+    filter: "all",
+  },
+  // 변수에 저장하고 재사용할 element 정보를 저장
+  element: {
+    todoInputElem: null,
+    listElem: null,
+    filtersElem: null,
+    clearCompletedBtn: null,
+  },
+  /**
+   * 최초 동작에 해야 할 일
+   */
+  initData() {
+    const savedTodos = localStorage.getItem("todos");
+    const savedFilter = localStorage.getItem("filter");
+    if (savedTodos) {
+      this.data.todos = JSON.parse(savedTodos);
+    }
 
-        if (savedFilter) {
-            this.data.filter = savedFilter;
-        }
-        // 이벤트 바인딩
-        this.bindEvent();
-        let thiz = this;
-        window.addEventListener("load", function() {
-            thiz.renderList();
-        });
-    },
-    bindEvent() {
-        const todoInputElem = document.querySelector(".new-todo");
-        let thiz = this;
-        todoInputElem.addEventListener('keydown', this.addTodoHandler.bind(this));
+    if (savedFilter) {
+      this.data.filter = savedFilter;
+    }
+  },
+  // element 정보 초기화
+  initElem() {
+    this.element.todoInputElem = document.querySelector(".new-todo");
+  },
 
-        const listElem = document.querySelector(".todo-list");
-        listElem.addEventListener("click", this.liTodoHandler.bind(this));
+  /**
+   * list element 참조 정보 저장
+   */
+  initListElem() {
+    this.element.listElem = document.querySelector(".todo-list");
+  },
+  initFiltersElem() {
+    this.element.filtersElem = document.querySelector(".filters");
+  },
+  initClearCompletedBtn() {
+    this.element.clearCompletedBtn = document.querySelector(".clear-completed");
+  },
+  init() {
+    // 초기화
+    this.initData();
+    // element 정보 초기화
+    this.initElem();
+    this.initListElem();
+    this.initFiltersElem();
+    this.initClearCompletedBtn();
+    // 이벤트 바인딩
+    this.bindEvent();
+    // 초기 랜더링
+    this.renderList();
+    this.renderFilter();
+  },
+  bindEvent() {
+    // element에서 가져와서 이벤트를 걸어준다.
+    this.element.todoInputElem.addEventListener(
+      "keydown",
+      this.addTodoHandler.bind(this)
+    );
 
-        const filtersElem = document.querySelector('.filters');
-        filtersElem.addEventListener("click", this.filterTodoHandler.bind(this));
+    this.element.listElem.addEventListener(
+      "click",
+      this.clickLiHandler.bind(this)
+    );
 
-        const clearCompletedBtn = document.querySelector('.clear-completed');
-        clearCompletedBtn.addEventListener('click',this.completedTodoHandler.bind(this));
-    },
-    // todo 아이템 추가
-    addTodoHandler(event) {
-        const todoInputElem = event.target;
-        if (event.key === 'Enter' && !event.isComposing && todoInputElem.value !== '') {
+    this.element.filtersElem.addEventListener(
+      "click",
+      this.filterTodoHandler.bind(this)
+    );
 
-            // 새로운 할 일 객체 생성
-            const newTodo = {
-                id: Date.now(), // 고유한 id 값으로 현재 시간 사용
-                title: todoInputElem.value,
-                completed: false
-            };
+    this.element.clearCompletedBtn.addEventListener(
+      "click",
+      this.completedTodoHandler.bind(this)
+    );
+  },
+  // todo 아이템 추가
+  addTodoHandler(event) {
+    const todoInputElem = event.target;
+    if (
+      event.key === "Enter" &&
+      !event.isComposing &&
+      todoInputElem.value !== ""
+    ) {
+      // 새로운 할 일 객체 생성
+      const newTodo = {
+        id: Date.now(), // 고유한 id 값으로 현재 시간 사용
+        title: todoInputElem.value,
+        completed: false,
+      };
 
-            // 할 일 목록에 추가
-            this.data.todos.push(newTodo);
+      // 할 일 목록에 추가
+      this.data.todos.push(newTodo);
 
-            // localStorage에 저장
-            localStorage.setItem('todos', JSON.stringify(this.data.todos));
+      // localStorage에 저장
+      this.savedTodosLocalStorage();
 
-            this.renderList();
-        }
-    },
-    liTodoHandler(event){
-        const targetElem = event.target;
-          
-        // li 엘리먼트 찾기 : closest() 메서드를 사용해서 li를 찾는다.
-        const liElem = targetElem.closest('li[data-id]');
-    
-        // 변경될 데이터(todo)를 찾기위한 id 정보
-        const id = liElem.dataset.id;
-    
-        // 삭제 버튼 클릭 시
-        if (targetElem.classList.contains("destroy")) {
-          // 데이터 변경 : id를 사용하여 배열 todos 에서 삭제할 todo를 제거
-          this.data.todos = this.data.todos.filter(todo => todo.id !== parseInt(id));
+      this.renderList();
+    }
+  },
+  clickLiHandler() {
+    const targetElem = event.target;
 
-          localStorage.setItem("todos", JSON.stringify(this.data.todos));
+    // li 엘리먼트 찾기 : closest() 메서드를 사용해서 li를 찾는다.
+    const liElem = targetElem.closest("li[data-id]");
 
-          this.renderList();
-        } 
-    
-        // 체크박스가 토글 시
-        else if (targetElem.classList.contains("toggle")) {
-          // 데이터 가져오기 : id를 사용하여 todo 데이터 가져오기
-          const todoItem = this.data.todos.find(todo => todo.id === parseInt(id));
-    
-          // 데이터 변경 : 가져온 todo 데이터에서 완료여부를 변경(반대 값으로) true -> false, false -> true
-          todoItem.completed = !todoItem.completed;
-    
-          localStorage.setItem("todos", JSON.stringify(this.data.todos));
-    
-          this.renderList(); 
-        }
-    },
-    filterTodoHandler(event){
-        const targetElem = event.target;
-        // 대상 찾기
-        const href = targetElem.getAttribute("href");
-        if(href === "#/"){
-          this.data.filter = 'all';
-        } else if(href === "#/active"){
-          this.data.filter = 'active';
-        } else if(href === "#/completed"){
-          this.data.filter = 'completed';
-        }
-        // 필터 영역 다시 그리기
-        this.renderFilter();
-        // 목록 영역 다시 그리기
-        this.renderList();
-    },
-    completedTodoHandler(event){
-        this.data.todos = this.data.todos.filter(item => !item.completed);
-        
-        localStorage.setItem('todos', JSON.stringify(this.data.todos));
-        this.renderList();
-    },
-    renderList() {
-        const todoInputElem = document.querySelector(".new-todo");
-        const listElem = document.querySelector(".todo-list");
-        const todosCountElem = document.querySelector('.todo-count');
-        let listHTML = "";
-        let filteredTodos = [];
-      
-        // 필터 조건에 맞게 목록을 가공하고
-        if (this.data.filter === 'all') {
-          filteredTodos = this.data.todos;
-        } else if (this.data.filter === 'active') {
-          filteredTodos = this.data.todos.filter(item => !item.completed);
-        } else if (this.data.filter === 'completed') {
-          filteredTodos = this.data.todos.filter(item => item.completed);
-        }
-        // 그 목록으로 화면을 그린다.
-        filteredTodos.forEach((item, index) => {
-          const completedClass = item.completed ? "completed" : "";
-          listHTML += `
+    // 변경될 데이터(todo)를 찾기위한 id 정보
+    const id = parseInt(liElem.dataset.id);
+
+    if (targetElem.classList.contains("destroy")) {
+      this.destroyHandler(id);
+    } else if (targetElem.classList.contains("toggle")) {
+      this.completedHandler(id);
+    }
+  },
+  destroyHandler(id) {
+    // 데이터 변경 : id를 사용하여 배열 todos 에서 삭제할 todo를 제거
+    this.data.todos = this.data.todos.filter((todo) => todo.id !== id);
+    // 로컬스토리지에 저장
+    this.savedTodosLocalStorage();
+
+    this.renderList();
+  },
+  completedHandler(id) {
+    // 데이터 가져오기 : id를 사용하여 todo 데이터 가져오기
+    const todoItem = this.data.todos.find((todo) => todo.id === id);
+
+    // 데이터 변경 : 가져온 todo 데이터에서 완료여부를 변경(반대 값으로) true -> false, false -> true
+    todoItem.completed = !todoItem.completed;
+    // 로컬스토리지에 저장
+    this.savedTodosLocalStorage();
+
+    this.renderList();
+  },
+  filterTodoHandler(event) {
+    const targetElem = event.target;
+    // 대상 찾기
+    const href = targetElem.getAttribute("href");
+    if (href === "#/") {
+      this.data.filter = "all";
+    } else if (href === "#/active") {
+      this.data.filter = "active";
+    } else if (href === "#/completed") {
+      this.data.filter = "completed";
+    }
+    // 필터를 로컬스토리지에 저장
+    this.savedFiltersLocalStorage();
+    // 필터 영역 다시 그리기
+    this.renderFilter();
+    // 목록 영역 다시 그리기
+    this.renderList();
+  },
+  completedTodoHandler(event) {
+    this.data.todos = this.data.todos.filter((item) => !item.completed);
+    // 로컬스토리지에 저장
+    this.savedTodosLocalStorage();
+
+    this.renderList();
+  },
+  savedTodosLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(this.data.todos));
+  },
+  savedFiltersLocalStorage() {
+    localStorage.setItem("filter", this.data.filter);
+  },
+  filterList() {
+    thiz = this;
+    let filteredTodos = [];
+    if (thiz.data.filter === "all") {
+      filteredTodos = thiz.data.todos;
+    } else if (thiz.data.filter === "active") {
+      filteredTodos = thiz.data.todos.filter((item) => !item.completed);
+    } else if (thiz.data.filter === "completed") {
+      filteredTodos = thiz.data.todos.filter((item) => item.completed);
+    }
+
+    // 배열 반환
+    return filteredTodos;
+  },
+  renderList() {
+    const todoInputElem = document.querySelector(".new-todo");
+    const listElem = document.querySelector(".todo-list");
+    const todosCountElem = document.querySelector(".todo-count");
+    let listHTML = "";
+
+    // filterList() 함수에서 반환된 배열을 참조
+    const filteredTodos = this.filterList();
+    // 그 목록으로 화면을 그린다.
+    filteredTodos.forEach((item, index) => {
+      const completedClass = item.completed ? "completed" : "";
+      listHTML += `
             <li data-id="${item.id}" class="${completedClass}">
               <div class="view">
-                <input class="toggle" type="checkbox" ${item.completed ? "checked" : ""}>
+                <input class="toggle" type="checkbox" ${
+                  item.completed ? "checked" : ""
+                }>
                 <label>${item.title}</label>
                 <button class="destroy"></button>
               </div>
             </li>
           `;
-        });
-      
-        const items = filteredTodos.length;
-        listElem.innerHTML = listHTML;
-        todosCountElem.innerHTML = `
-          <strong>${items}</strong>
-          items left
-        `
+    });
 
-        todoInputElem.value = "";
-    },
-    renderFilter() {
-        const filtersElem = document.querySelector('.filters');
-        const filterHTML = `
+    const items = filteredTodos.length;
+    listElem.innerHTML = listHTML;
+    todosCountElem.innerHTML = `<strong>${items}</strong> items left`;
+
+    todoInputElem.value = "";
+  },
+  renderFilter() {
+    const filtersElem = document.querySelector(".filters");
+    const filterHTML = `
         <li>
-          <a href="#/" class="${this.data.filter === 'all' ? 'selected' : ''}">All</a>
+          <a href="#/" class="${
+            this.data.filter === "all" ? "selected" : ""
+          }">All</a>
         </li>
         <li>
-          <a href="#/active" class="${this.data.filter === 'active' ? 'selected' : ''}">Active</a>
+          <a href="#/active" class="${
+            this.data.filter === "active" ? "selected" : ""
+          }">Active</a>
         </li>
         <li>
-          <a href="#/completed" class="${this.data.filter === 'completed' ? 'selected' : ''}">Completed</a>
+          <a href="#/completed" class="${
+            this.data.filter === "completed" ? "selected" : ""
+          }">Completed</a>
         </li>
         `;
-      
-        filtersElem.innerHTML = filterHTML;
-    }
-}
 
-TodoList.init();
+    filtersElem.innerHTML = filterHTML;
+  },
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  TodoList.init();
+});
